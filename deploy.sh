bfe946c049db443ab37be3be18fcb4300fa3ab81
Andrey Pozolotin <posandrey@gmail.com>
2023-05-12 00:47:06 +0200

    Fixed docker compose

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git pull
 
 VERSION=$(cat VERSION)
 echo "VERSION ${VERSION}"
-docker-compose build --build-arg version=${VERSION} chatgpt
+docker-compose build --build-arg version=${VERSION} chatgpt --no-cache
 docker-compose stop chatgpt
 docker-compose up -d chatgpt
