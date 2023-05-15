9735f7382b6e386ec69725de5603c72cc54f263a
Andrey Pozolotin <posandrey@gmail.com>
2023-05-15 18:25:10 +0200

    Don't fail on missing cfg files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"breathbathChatGPT/pkg/cmd"
 	"breathbathChatGPT/pkg/errs"
+	"breathbathChatGPT/pkg/utils"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
@@ -10,7 +11,14 @@ import (
 func main() {
 	logrus.SetLevel(logrus.DebugLevel)
 
-	err := godotenv.Overload(".env.default", ".env.secret", ".env.local")
+	cfgFiles := []string{".env.default", ".env.secret", ".env.local"}
+	existingFiles := make([]string, 0, len(cfgFiles))
+	for _, cfgF := range cfgFiles {
+		if utils.FileExists(cfgF) {
+			existingFiles = append(existingFiles, cfgF)
+		}
+	}
+	err := godotenv.Overload(existingFiles...)
 	errs.Handle(err, true)
 
 	err = cmd.Execute()
